f25bdc92bd152655eb60fc290a21f20a843efa04
Alexanski13 <alexanderivanov344@gmail.com>
2023-02-26 14:55:36 +0200

    init

---
+++ servlet-demo/src/main/java/softuni/servletdemo/MyCustomFilter.java
@@ -0,0 +1,25 @@
+package softuni.servletdemo;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "MyCustomFilter",
+servletNames = "helloServlet")
+public class MyCustomFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        ((HttpServletRequest)request).getSession().setAttribute("name", "Pesho");
+
+        chain.doFilter(request, response);
+    }
+}

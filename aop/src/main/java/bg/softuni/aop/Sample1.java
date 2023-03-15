987f36d0619403d81049f732cb5434b7ea464c48
Alexanski13 <alexanderivanov344@gmail.com>
2023-03-15 19:02:23 +0200

    init

---
+++ aop/src/main/java/bg/softuni/aop/Sample1.java
@@ -0,0 +1,35 @@
+package bg.softuni.aop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample1 implements CommandLineRunner {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(Sample1.class);
+
+    private final SampleComponent sampleComponent;
+
+    public Sample1(SampleComponent sampleComponent) {
+        this.sampleComponent = sampleComponent;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        sampleComponent.sayHello();
+        sampleComponent.echoSomething("I'm a demo");
+//        try {
+//            sampleComponent.doSomethingWrong();
+//        } catch (Exception e) {
+//            LOGGER.error("Ups an error with message {}", e.getMessage());
+//        }
+
+        String s1 = "string1";
+        String s2 = "string2";
+        String result = sampleComponent.concatTwoStrings(s1, s2);
+
+        LOGGER.info("Concatenating {} and {}. Result is {}", s1, s2, result);
+    }
+}
